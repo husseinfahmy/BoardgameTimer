c215e41278be8f54b65430c69a290e2dc38450ce
Hussein Fahmy <fahmy.hussein@hotmail.com>
2016-05-04 17:51:51 -0400

    first page and user input done

---
+++ app/src/main/java/tech/husseinfahmy/boardgametimer/MainActivity.java
@@ -0,0 +1,79 @@
+package tech.husseinfahmy.boardgametimer;
+
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    //local vars for timer info, add getters and setters after
+    private int numPlayers;
+    private int bankMins;
+    private int bankSecs;
+    private int roundMins;
+    private int roundSecs;
+
+
+
+    private double bankTime;
+    private double roundTime;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button) findViewById(R.id.startTimerButton);
+
+
+        submitButton.setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        try {
+                            EditText numPlayersField = (EditText) findViewById(R.id.numPlayersField);
+                            numPlayers = Integer.parseInt(numPlayersField.getText().toString());
+
+                            EditText bankMinsField = (EditText) findViewById(R.id.bankMinsField);
+                            bankMins = Integer.parseInt(bankMinsField.getText().toString());
+
+                            EditText bankSecsField = (EditText) findViewById(R.id.bankSecsField);
+                            bankSecs = Integer.parseInt(bankSecsField.getText().toString());
+
+                            EditText roundMinsField = (EditText) findViewById(R.id.roundMinsField);
+                            roundMins = Integer.parseInt(roundMinsField.getText().toString());
+
+                            EditText roundSecsField = (EditText) findViewById(R.id.roundSecsField);
+                            roundSecs = Integer.parseInt(roundSecsField.getText().toString());
+
+                            if ((numPlayers <= 0) || (bankMins < 0) || (bankSecs < 0) ||(roundMins<0)||(roundMins<0)||(roundSecs>59)||(bankSecs>59)) {
+                                Toast errorMessage = Toast.makeText(getApplicationContext(),"Invalid Field(s)",Toast.LENGTH_SHORT);
+                                errorMessage.show();
+                            }
+                            else{
+                                roundTime = (roundMins*60000)+(roundSecs*1000);
+                                bankTime = (bankMins*60000)+(bankSecs*1000);
+                            }
+                        }catch(NumberFormatException nfe){
+                            Toast errorMessage = Toast.makeText(getApplicationContext(),"Invalid Field(s)",Toast.LENGTH_SHORT);
+                            errorMessage.show();
+                        }
+                    }
+                }
+
+        );
+    }
+
+
+    public double getRoundTime() {
+        return roundTime;
+    }
+
+    public double getBankTime() {
+        return bankTime;
+    }
+
+}
